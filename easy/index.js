3cd00e611ac19518862dc9af848b9fb054f580c7
mads-byte <mbutl224@icloud.com>
2025-08-03 02:51:42 -0400

    reworked easy challenge

--- easy/index.js
+++ easy/index.js
@@ -1,17 +1,22 @@
-function takeExercise(exercise) {
-    let curExercise = exercise;
-
-    function reportExercise() {
-        let todayExercise = `Today's exercise: ${curExercise}`;
-        return todayExercise;
+function exerciseDay(today, deal) {
+    let day = {
+        dayName: today,
+        dailyDeal: deal,
+    }
+    return {
+        exercise: function (activity) {
+            return `Today's exercise: ${activity}. Today's deal: ${day.dailyDeal}`
+        }
     };
-
-    return reportExercise();
 };
 
-const exercise = takeExercise;
+let Monday = exerciseDay("Monday", "Bring a Friend");
+let Tuesday = exerciseDay("Tuesday", "50% off Merch");
+let Wednesday = exerciseDay("Wednesday", "Free Cycling Class");
+let Thursday = exerciseDay("Monday", "$2 Key Chains");
 
-console.log(exercise("Running"));
-console.log(exercise("Swimming"));
-console.log(exercise("Dancing"));
-console.log(exercise("Fencing"));
+
+console.log(Monday.exercise("Running"));
+console.log(Tuesday.exercise("Swimming"));
+console.log(Wednesday.exercise("Dancing"));
+console.log(Thursday.exercise("Fencing"));
