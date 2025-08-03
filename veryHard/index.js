7fb4ddf354bb97dd6dc141d53caff086659f2f89
mads-byte <mbutl224@icloud.com>
2025-08-03 00:41:07 -0400

    add methods to programmer constructor

--- veryHard/index.js
+++ veryHard/index.js
@@ -10,9 +10,38 @@ class Person {
     exercise() {
         console.log("I walk 10,000 steps a day!")
     }
+    fetchJob() {
+        console.log(`${this.name} is a ${this.job}`)
+    }
 };
-const person3 = new Person("Jack", "Web Dev", "29");
+
+class Programmer extends Person {
+    constructor(name, job, age, languages) {
+        super(name, job, age); //In a derived class such as this one, "super()" must be called before the use of "this"
+        this.languages = languages;
+        this.busy = true;
+    }
+    completeTask() {
+        this.busy = false;
+        console.log(this.busy);
+    }
+    acceptNewTask() {
+        this.busy = true;
+        console.log(this.busy);
+    }
+    offerNewTask() {
+        if (this.busy == true) {
+            console.log(`${this.name} can't accept any new tasks right now`)
+        }
+        if (this.busy == false) {
+            console.log(`${this.name} would love to take on a new responsibility.`)
+        }
+    }
+}
 
 
-person3.exercise();
+const person1 = new Programmer("Harold", "Backend Engineer", 20, ["HTML", "CSS", "JS"]);
+
+person1.completeTask();
+person1.offerNewTask();
 
